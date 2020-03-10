8d4f8a871014ed87587763b54a24781d29d3a2a6
james vreeland <j@mesv.org>
2020-03-10 13:51:18 -0400

    unknown caeser offset loop

--- maris_2020/cipher_01.py
+++ maris_2020/cipher_01.py
@@ -6,8 +6,12 @@ ciphers_01 = [
     "PELRIAOBXA"
 ]
 
+#for offset in range(0, 26):
+
 offset = 23
 passphrase = cipher_utils.caesar_offset_passphrase(offset)
 
 for i in ciphers_01:
     print cipher_utils.caesar_decode(i, passphrase)
+
+#print "------"
